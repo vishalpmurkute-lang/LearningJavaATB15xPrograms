5ac90fc8581feefa8ebdc120206b101b806cd504
Promode <pramoddutta_96@yahoo.com>
2026-01-19 07:56:38 +0530

    19th Jan 2025

---
+++ src/ex_16_Arrays/Lab164_2nd_highlest_Array.java
@@ -0,0 +1,25 @@
+package ex_16_Arrays;
+
+public class Lab164_2nd_highlest_Array {
+    public static void main(String[] args) {
+
+        int[] numbers = {12, 45, 67, 23, 89, 45, 89};
+        int highest = 0;
+        int secondHighest = 0;
+        // 67
+        for (int num : numbers) {
+            if (num > highest) {
+                secondHighest = highest;
+                highest = num;
+            } else if (num > secondHighest && num != highest) {
+                secondHighest = num;
+            }
+
+        }
+        System.out.println(secondHighest);
+
+    }
+
+
+}
+
---
+++ src/ex_16_Arrays/Lab166_BufferReaderInput.java
@@ -0,0 +1,18 @@
+package ex_16_Arrays;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Lab166_BufferReaderInput {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the value if N");
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+//        Scanner scanner = new Scanner(System.in);
+//        int N1 = scanner.nextInt();
+
+        System.out.println(N);
+    }
+}
